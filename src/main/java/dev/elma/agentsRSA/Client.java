db4e4bec30efaceaa634168f424a9759ab13e963
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-04-14 00:06:52 +0000

    commit

--- src/main/java/dev/elma/agentsRSA/Client.java
+++ src/main/java/dev/elma/agentsRSA/Client.java
@@ -7,19 +7,26 @@ import jade.lang.acl.ACLMessage;
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 
 public class Client extends Agent {
     @Override
     protected void setup() {
         //get the secret key in string format
-        PublicKey publicKey = (PublicKey) getArguments()[0];
+        String publicKey = (String) getArguments()[0];
+        byte[] decode = Base64.getDecoder().decode(publicKey);
+
 
         try {
+            KeyFactory keyFactory=KeyFactory.getInstance("RSA");
+            PublicKey publicKeyGenerate = keyFactory.generatePublic(new X509EncodedKeySpec(decode));
 
             Cipher cipher=Cipher.getInstance("RSA");
-            cipher.init(Cipher.ENCRYPT_MODE,publicKey);
+            cipher.init(Cipher.ENCRYPT_MODE,publicKeyGenerate);
             //crypt message
             byte[] cryptMessage = cipher.doFinal("Hello Server Are you Here?".getBytes());
             //byte to string encoded
